49961adf76b4daf5bd5dc0b214d9be5a1c3e82f5
tahmid <tahmidrahman2812@outlook.com>
2023-02-25 23:52:07 +0000

    feat: converts message into json format

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import websocket
+import websocket, json, pprint
 
 # Create a socket that retrives data on ETHUSDT pair every 1 minute
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
@@ -11,9 +11,14 @@ def on_close(ws):
     
 def on_message(ws, message):
     print("Message Recieved")
-    print(message)
+    json_message = json.loads(message)
+    pprint.pprint(json_message)
+
     
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 
-ws.run_forever()
+try:
+    ws.run_forever()
+except KeyboardInterrupt:
+    on_close(ws)
