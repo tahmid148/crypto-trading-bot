1d027e8efdf9df34e03f4bd076ddd8c4eba95a38
tahmid <tahmidrahman2812@outlook.com>
2023-02-26 00:12:19 +0000

    feat: closed candles are tracked into an array

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import websocket, json, pprint
 # Create a socket that retrives data on ETHUSDT pair every 1 minute
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
+closes = []
+
 def on_open(ws):
     print("Connection Opened")
 
@@ -20,6 +22,8 @@ def on_message(ws, message):
     
     if is_candle_closed:
         print(f"Candle closed at {close_price}")
+        closes.append(float(closes))
+        print(f"Closes: {closes}")
     
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
