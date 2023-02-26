6a3caabc6813febfbac558a94ed598e273d5826e
tahmid <tahmidrahman2812@outlook.com>
2023-02-26 00:43:24 +0000

    feat: RSI gets calculated and decides to buy and sell

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ TRADE_SYMBOL = "ETHUSD"
 TRADE_QUANTITY = 0.05
 
 closes = []
+in_position = False
 
 def on_open(ws):
     print("Connection Opened")
@@ -31,6 +32,27 @@ def on_message(ws, message):
         print(f"Candle closed at {close_price}")
         closes.append(float(closes))
         print(f"Closes: {closes}")
+        
+        if (len(closes) > RSI_PERIOD):
+            np_closes = np.array(closes)
+            # Calculates RSI Values
+            rsi = talib.RSI(np_closes, RSI_PERIOD) 
+            print(f"RSI's calculated so far: {rsi}")
+            
+            last_rsi = rsi[-1]
+            print(f"Current RSI: {last_rsi}")
+            if (last_rsi > RSI_OVERBOUGHT):
+                if in_position:
+                    print("Overbought: SELL!")
+                    # Insert Binance Logic
+                else:
+                    print("Overbought, but we have no position")
+    
+            elif (last_rsi < RSI_OVERSOLD):
+                if (in_position):
+                    print("It is oversold, but you already own it!")
+                print("Oversold: BUY!")
+                # Insert Binance Logic
     
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
